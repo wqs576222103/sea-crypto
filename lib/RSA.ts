6d91da1d7592054f1ed1ed285109f7c8fd10ab93
王清山 <576222103@qq.com>
2022-04-02 15:31:47 +0800

    fixL RSA生成sign方法、加解密方法调整

--- lib/RSA.ts
+++ lib/RSA.ts
@@ -5,16 +5,16 @@ const RSA_CONFIG = {
   hash: { name: "SHA-512" },
 };
 
-export const RSAKeyPair: TRSAKeyPair = {
+export const clientRSAKeyPair: TRSAKeyPair = {
   PRIVATE_KEY:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCHQrZ0kuI9midV2Ky08TIdxDbL6wsiQcsApG90SIZhWBCs42lfX8Gvyn7RRBKw9KoYRwshybLbnfLKEb/t9BS0zKtzxnLOL2GAXhSgNWXAV7YMQuiy9ScvpS2uiBL6OA7eP5+qmRtGh41FZs9GbTiYg2AvpZkgvnJsALP3TteRXoujVnq3MKTZpzfcFAiHIdWz9gIhZYfWRnZUamHK3Xyiysk/6QLs3n8nXjDRc2UyOVUmryl4uhNJ8p/Q3AnlXtWpyT3yImq/JS8/L7GRUuDD9EOdlngAlkcDp1gCfclJQcyoTsA6Yc1p0p3NmcbVWeUGZ9O3SsjEW2ckwNpaWDe5AgMBAAECggEAAfZ23VQwUkKFZZqCTxHc70+kl+ruO24G+xhnwaE3yfvUKh4WrlqeXeWrV3AMcWLrgPSu9auOm9874bDGPza2gjUiG8j6a4GGga1b0UNVGahsLNtfsxEZo3hbJFg3Jkhf9tunfntASRK4exRV16jIFHZ7k9VsVmxNDsIn2mPbfvGBv1yCtlsuCiBlZ58faQzitQmuhd+N50OV6M8+3NIwgOKp9VTWqHEWIU9INxaTtoplL3x7u9nkuN9Hn5Y2XQGKulxyVduj549v9/3Wh41GpisQvK4fQxs9erg+bUy+mCUr8k5TFh9WdpJ+gF2dX2LTpodoKH0Up6ZtV2usQ5s9gQKBgQDnrKaKM1rvLqVIJjdECg/ygdkOpwgsNsZCS3Mb0eAJcaZAs7sotMGiVj+c4Qe+QbwnLILqW83ggrBK/YhpL0Nn1nGorFTkEb7yGBv4AzITl2WjSInirjjD4ALvrM6o6s60w/Olp/MMrCenH0aX3ZG+jQu61miFyPfzw5m127F+kQKBgQCVdnuyXi1b8fTfSPetj5B44t3PQdBcv6wI27NBpcvU4005Iz/kGpITWxpmE72KGrkDeiCcs4AZRLOG7ko4NOvamsPhGjNYDa1ukXWR4RdTYWUyTUfSGOR89Wp9/ias6n3WFLy22exOoOA+UsTaLDhkj6/mTH7kTKlxRFh9QJoKqQKBgE3nmuEiP9PqZZ4La84vbAlqbKkmtfLYQHcOlBiozKOgWf2r8qXPksWaJy4D5eyugizSJPvV/zcd+1ZcR0DHqe0DCZlkye8foIwcW1tdB7vaz0zHGcbmOyRy8cnS3HOk8fmLYyUzguGiCnuCYuSyl03ydB1R+Df0ypxoDApBZEBhAoGAePUwWkPZVaXU1LdKqo2bEBoaBFdw9v6vjWOwCJrTAPELWAhs9n+CZGPAU4f6RsAlpui5Z9fk/Y4Z9EL4kdBSZ9IEYDJCByrahrb1fR+7LuGNck4Up7U5hN9gVaLPTfVf7VdO+nQWx/NuR0HYyfArm0alxi12K/DW2DHFo2gawjECgYAIfSyU+9YmqGiRmBb8VGh8lx8SpL0f0iOjTBZnSNPtRUSLMwCnATBvscUsVpYCaUvDVDwsVdvk5XxZ53Kp6UZHyWqTiskI7LT4dioIdbQfuW2gC2KJjMzCOcjAfS+9GyUhhA1Y1mlv5rscqMPNMwOkOJtBIDXZcI5+oIb0DL1FUw==\n-----END PRIVATE KEY-----",
+    "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDVLTa9+5ZGqiaprnbRtUxISDK1NlC3m/ovbVAZ2fYkf4BLSF5iWyDqcQzUmk0lJjb8Tdi3FxydCHzFz6NN58NiW0/oMy8SO7eFNw1GMOismZWGdSAOOxtm7rHPe/2rnYVtlgqZtJzM6pDuNyYDNDwg8AsbUwdKopoKgud18fYg3R+FQ5xXQrLesb79k/beU+Mhg3FCWrcbtqQY80g48XVDa3Fji/AklZvY3FAKF9L7sb7R1mySvm0wStRPCIEMAEaZloza6F2faWw3mlUzd0epKKfsnOVI3/+5Ele+dPzY0iXE8akC/HpmJ/j7LEI3T9Qc7RXzN9eRSkVxRhFJxLLAgMBAAECggEAFp5ndvfA1PaeUZI+8WJpa4//gR1Wr0ScxuG47GVNBCobtL0qJC3CsBhnjN3dKuaOMOHk3QDRf7ts9v5ZEvc+5s1kJjsTmjYD2xmlSky80sk/ynahQdvVnPeskEfe0rzvFaKQ4RAZDJgN/luYx86sHz3SchKR/sqfTmKYB8Yl8S9L61SxnTIxbg9KZXIGtX2tcbn0TIFWUp6dwd2/6mbkKXHswhaj/0A9LW/7TM9JDO8lXLh/xFapgquJaP1NmLMsUwUsDUUoGNGRcBBdQcZUpkht8zDHTk4UNOrZztayQARwRSDOasreHgFoMijEbWNw3+uAvKLqFq0Sbs2s+UtVaQKBgQDnW5iOfeS8aO8jDEC2UkZJnvaZQ8Sg1u6kp4TpG4S+I87kJVBGrLQ9NHaK15qh+glLk9OUJlQp2LN5pd8IGaIyJhhDkwsbTkNZz0wNu83IfjgKheXEgNAOwsvMiwMmiJ7hKZKhOmqIZmLlXhGvccTOcmD887VHbs9rkUu84RGQFwKBgQDYIsTtLq2MrmulZVJ8pJx5jRqrpJl3AYW65xl1Z75F6yDTaHe52uJvh7rqodAHl7eYWf9Xrwgwhvoo7oCI7nmItzKglGUQzP28qcAOJFTno8t6MUDPW+2eZ0WhtWsNqsTvHduzcTX5ub0PCevrn/Ae/De7q54ylNj6rf7p4VuvbQKBgQDANUG+B+tzeD5MQM62ag4I9e7rj/iYo1fqJV5vh7Q1f2xP8IwL20l2m9JsMQk/zonQZ0TdDSbZCTa6NsrfYOR2g12fLleI3PFpeKY3trIVAn+uhmVIZrfzu3QzP75jUhek+rPeDLkbDuzUn91Tg9I8rZvUKuiNKwtZ04E3r0i3CwKBgCPrw2bX9ygGZQWcDVWB1i+tb91PAeJxclRZzDVjBJCCP3ZjSmKtKiz3Fe2g7v4UDQNZEcL06BuIk1LyMJKRb8l1CQj/ILzmFNWiMLUo/KNy6YJpZSgRUzCcYUo66Ivg8dH72GO+UQYsBGvXbko+zp2XtXuPTj+B1X30JQWh2qcRAoGBAMLjMvc5SyCIwADSOVJp/EysTAs6gUmYMGFPPQfYkukt883x7oBPMqIG6+RpdF4w+tFDJQIO+XIoVmlDwNJ558vJkFMW4h97ZkpukbUUxlcSadb7u2yYP/bYSItXqE5z2FmBsV2G0e/33mALY75mlagm1tEVNZY/12KhuuKSxqAB",
   PUBLIC_KEY:
-    "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh0K2dJLiPZonVdistPEyHcQ2y+sLIkHLAKRvdEiGYVgQrONpX1/Br8p+0UQSsPSqGEcLIcmy253yyhG/7fQUtMyrc8Zyzi9hgF4UoDVlwFe2DELosvUnL6UtrogS+jgO3j+fqpkbRoeNRWbPRm04mINgL6WZIL5ybACz907XkV6Lo1Z6tzCk2ac33BQIhyHVs/YCIWWH1kZ2VGphyt18osrJP+kC7N5/J14w0XNlMjlVJq8peLoTSfKf0NwJ5V7Vqck98iJqvyUvPy+xkVLgw/RDnZZ4AJZHA6dYAn3JSUHMqE7AOmHNadKdzZnG1VnlBmfTt0rIxFtnJMDaWlg3uQIDAQAB\n-----END PUBLIC KEY-----",
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw1S02vfuWRqomqa520bVMSEgytTZQt5v6L21QGdn2JH+AS0heYlsg6nEM1JpNJSY2/E3YtxccnQh8xc+jTefDYltP6DMvEju3hTcNRjDorJmVhnUgDjsbZu6xz3v9q52FbZYKmbSczOqQ7jcmAzQ8IPALG1MHSqKaCoLndfH2IN0fhUOcV0Ky3rG+/ZP23lPjIYNxQlq3G7akGPNIOPF1Q2txY4vwJJWb2NxQChfS+7G+0dZskr5tMErUTwiBDABGmZaM2uhdn2lsN5pVM3dHqSin7JzlSN//uRJXvnT82NIlxPGpAvx6Zif4+yxCN0/UHO0V8zfXkUpFcUYRScSywIDAQAB",
 };
 
-export const serverRSAKey = {
+export const serverRSAKeyPair = {
   PUBLIC_KEY:
-    "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh0K2dJLiPZonVdistPEyHcQ2y+sLIkHLAKRvdEiGYVgQrONpX1/Br8p+0UQSsPSqGEcLIcmy253yyhG/7fQUtMyrc8Zyzi9hgF4UoDVlwFe2DELosvUnL6UtrogS+jgO3j+fqpkbRoeNRWbPRm04mINgL6WZIL5ybACz907XkV6Lo1Z6tzCk2ac33BQIhyHVs/YCIWWH1kZ2VGphyt18osrJP+kC7N5/J14w0XNlMjlVJq8peLoTSfKf0NwJ5V7Vqck98iJqvyUvPy+xkVLgw/RDnZZ4AJZHA6dYAn3JSUHMqE7AOmHNadKdzZnG1VnlBmfTt0rIxFtnJMDaWlg3uQIDAQAB\n-----END PUBLIC KEY-----",
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh0K2dJLiPZonVdistPEyHcQ2y+sLIkHLAKRvdEiGYVgQrONpX1/Br8p+0UQSsPSqGEcLIcmy253yyhG/7fQUtMyrc8Zyzi9hgF4UoDVlwFe2DELosvUnL6UtrogS+jgO3j+fqpkbRoeNRWbPRm04mINgL6WZIL5ybACz907XkV6Lo1Z6tzCk2ac33BQIhyHVs/YCIWWH1kZ2VGphyt18osrJP+kC7N5/J14w0XNlMjlVJq8peLoTSfKf0NwJ5V7Vqck98iJqvyUvPy+xkVLgw/RDnZZ4AJZHA6dYAn3JSUHMqE7AOmHNadKdzZnG1VnlBmfTt0rIxFtnJMDaWlg3uQIDAQAB",
 };
 
 /*
@@ -27,11 +27,7 @@ async function cryptoKey2Base64Key(key: CryptoKey, isPrivate: boolean) {
       key
     );
     const exportedAsBase64 = ab2base64Str(exported);
-    const keyType = isPrivate ? "PRIVATE" : "PUBLIC";
-    const pemExported = `-----BEGIN ${keyType} KEY-----\n${exportedAsBase64
-      .replace(/[^\x00-\xff]/g, "$&\x01")
-      .replace(/.{64}\x01?/g, "$&\n")}\n-----END ${keyType} KEY-----`;
-    return pemExported;
+    return exportedAsBase64;
   } catch (error) {
     console.log(error);
   }
@@ -42,16 +38,8 @@ async function cryptoKey2Base64Key(key: CryptoKey, isPrivate: boolean) {
  base64(pem)格式转CryptoKey
  */
 function base64Key2CryptoKey(pem: string, isPrivate: boolean) {
-  // fetch the part of the PEM string between header and footer
-  const keyType = isPrivate ? "PRIVATE" : "PUBLIC";
-  const pemHeader = `-----BEGIN ${keyType} KEY-----\n`;
-  const pemFooter = `\n-----END ${keyType} KEY-----`;
-  const pemContents = pem.substring(
-    pemHeader.length,
-    pem.length - pemFooter.length
-  );
   // convert from a binary string to an ArrayBuffer
-  const binaryDer = base64Str2ab(pemContents);
+  const binaryDer = base64Str2ab(pem);
 
   return window.crypto.subtle.importKey(
     isPrivate ? "pkcs8" : "spki",
@@ -66,23 +54,10 @@ function base64Key2CryptoKey(pem: string, isPrivate: boolean) {
 }
 
 async function savePublicKey(publicKey: CryptoKey) {
-  RSAKeyPair.PUBLIC_KEY = await cryptoKey2Base64Key(publicKey, false);
+  clientRSAKeyPair.PUBLIC_KEY = await cryptoKey2Base64Key(publicKey, false);
 }
 async function savePrivateKey(privateKey: CryptoKey) {
-  RSAKeyPair.PRIVATE_KEY = await cryptoKey2Base64Key(privateKey, true);
-}
-
-async function getCryptoPrivateKey() {
-  if (RSAKeyPair.PRIVATE_KEY !== null) {
-    return base64Key2CryptoKey(RSAKeyPair.PRIVATE_KEY, true);
-  }
-  return null;
-}
-async function getCryptoPublicKey() {
-  if (RSAKeyPair.PUBLIC_KEY !== null) {
-    return base64Key2CryptoKey(RSAKeyPair.PUBLIC_KEY, false);
-  }
-  return null;
+  clientRSAKeyPair.PRIVATE_KEY = await cryptoKey2Base64Key(privateKey, true);
 }
 
 /*
@@ -102,45 +77,79 @@ export const generateRSAKey = async () => {
   );
   await savePublicKey(keyPair.publicKey);
   await savePrivateKey(keyPair.privateKey);
-  return RSAKeyPair;
+  return clientRSAKeyPair;
+};
+
+export const base64Key2CryptoSignKey = (
+  key: string,
+  isPrivate: boolean,
+  keyUsages: KeyUsage[]
+) => {
+  const binaryDer = base64Str2ab(key);
+  // @ts-ignore
+  return window.crypto.subtle.importKey(
+    isPrivate ? "pkcs8" : "spki",
+    binaryDer,
+    {
+      name: "RSASSA-PKCS1-v1_5",
+      // Consider using a 4096-bit key for systems that require long-term security
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-256",
+    },
+    true,
+    keyUsages
+  );
 };
 
 /*
  生成签名
  */
 export const generateRSASign = async (data: string) => {
-  const privateKey = await getCryptoPrivateKey();
-  let enc = new TextEncoder();
-
+  const privateKey = await base64Key2CryptoSignKey(
+    clientRSAKeyPair.PRIVATE_KEY || "",
+    true,
+    ["sign"]
+  );
   // const digestbuffer = await crypto.subtle.digest('SHA-256', str2ab(JSON.stringify(data)));
   if (privateKey !== null) {
     let buffer = await window.crypto.subtle.sign(
-      RSA_CONFIG.name,
+      "RSASSA-PKCS1-v1_5",
       privateKey,
-      enc.encode('data')
+      str2ab(data)
     );
     return ab2base64Str(buffer);
   }
   return null;
 };
+/**
+ * 验证签名
+ */
+export const verifySign = async (sign: string, data: string) => {
+  const publicKey = await base64Key2CryptoSignKey(
+    clientRSAKeyPair.PUBLIC_KEY || "",
+    false,
+    ["verify"]
+  );
+  return window.crypto.subtle.verify(
+    "RSASSA-PKCS1-v1_5",
+    publicKey,
+    base64Str2ab(sign),
+    str2ab(data)
+  );
+};
 
 /*
  加密
  */
-export const RSAEncrypt = async (data: any) => {
-  if (RSAKeyPair.PUBLIC_KEY === null) {
-    console.error(
-      "❌ please run generateRSAKey() to generate PUBLIC_KEY before decrypt data "
-    );
-    return;
-  }
-  const publicKey = await getCryptoPublicKey();
-  if (publicKey !== null) {
+export const RSAEncrypt = async (publicKey: string, data: any, ) => {
+  const bufferKey = await base64Key2CryptoKey(publicKey, false);
+  if (bufferKey !== null) {
     const buffer = await window.crypto.subtle.encrypt(
       {
         name: RSA_CONFIG.name,
       },
-      publicKey,
+      bufferKey,
       str2ab(JSON.stringify(data))
     );
     return ab2base64Str(buffer);
@@ -150,20 +159,15 @@ export const RSAEncrypt = async (data: any) => {
 /*
  解密
  */
-export const RSADecrypt = async (text: string) => {
-  if (RSAKeyPair.PRIVATE_KEY === null) {
-    console.error(
-      "❌ please run generateRSAKey() to generate PRIVATE_KEY before decrypt data "
-    );
-    return;
-  }
-  const privateKey = await getCryptoPrivateKey();
+export const RSADecrypt = async (privateKey: string, text: string) => {
+  const bufferKey = await base64Key2CryptoKey(privateKey, true);
+ 
   if (privateKey !== null) {
     const buffer = await window.crypto.subtle.decrypt(
       {
         name: RSA_CONFIG.name,
       },
-      privateKey,
+      bufferKey,
       str2ab(window.atob(text))
     );
     return ab2str(buffer);
--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,20 @@
 // @ts-ignore
 import { AESEncrypt, AESDecrypt, generateAESKey } from "./AES.js";
 // @ts-ignore
-import { RSAEncrypt, RSADecrypt, RSAKeyPair, generateRSASign } from "./RSA.js";
-import { str2ab, abConcatenate, ab2base64Str, base64Str2ab } from "./utils.js";
-
+import {
+  RSAEncrypt,
+  // RSADecrypt,
+  // clientRSAKeyPair,
+  serverRSAKeyPair,
+  generateRSASign,
+} from "./RSA.js";
+import {
+  str2ab,
+  abConcatenate,
+  ab2base64Str,
+  base64Str2ab,
+  ab2str,
+} from "./utils.js";
 
 export const encrypt = async (data: any) => {
   // 生成签名
@@ -12,7 +23,7 @@ export const encrypt = async (data: any) => {
   const timestemp = String(new Date().getTime());
 
   // 组合 时间戳、签名、数据
-  const hybridData = abConcatenate(
+  const hybridBuffer = abConcatenate(
     str2ab(timestemp),
     base64Str2ab(sign),
     str2ab(data)
@@ -23,18 +34,19 @@ export const encrypt = async (data: any) => {
   // 生成Nonce
   const nonce = ab2base64Str(window.crypto.getRandomValues(new Uint8Array(16)));
   // 加密数据
-  let encrypt = await AESEncrypt(AESKey, hybridData, nonce);
+  let encrypt = await AESEncrypt(AESKey, ab2str(hybridBuffer), nonce);
   // 组合 Nonce、加密数据、用公钥加密的AESKey
 
-  const encryptAESKey = (await RSAEncrypt(AESKey)) || "";
+  const encryptAESKey =
+    (await RSAEncrypt(serverRSAKeyPair.PUBLIC_KEY, AESKey)) || "";
 
   const result = abConcatenate(
     base64Str2ab(nonce),
     base64Str2ab(encrypt),
     base64Str2ab(encryptAESKey)
   );
-  console.log(result);
-  return result;
+
+  return ab2base64Str(result);
 };
 
 export const decrypt = (text: string) => {};
