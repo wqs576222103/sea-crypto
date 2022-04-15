f7b656769513ad571273ecba7184445aab45c2ff
王清山 <576222103@qq.com>
2022-04-15 10:37:03 +0800

    feat: 不同系统设置环境变量打包

---
+++ typings/env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMetaEnv {
+    readonly VITE_CLIENT_PRIVATE_KEY: string
+    readonly VITE_CLIENT_PUBLIC_KEY: string
+    readonly VITE_SERVER_PUBLIC_KEY: string
+    // 更多环境变量...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
